32e411a41748cf282d891cfd14e0109d8e1ca515
Peter Schüllermann <Peter.schuellermann>
2017-06-02 16:14:44 +0200

    update benchmark.sh to also call memsweep

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,7 @@
 RUNTIME=300
 echo "Runtime is" $RUNTIME "seconds for linpack and memsweep each!"
-sh linpack.sh &
-TASK_PID=$!
-sleep $RUNTIME
-kill $TASK_PID
+sh linpack.sh & > linpack_results.txt
+#TASK_PID=$!
+#sleep $RUNTIME
+#kill $TASK_PID
+sh memsweep.sh > memsweep_results.txt
